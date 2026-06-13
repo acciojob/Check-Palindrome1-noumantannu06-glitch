4bc7236ce580ca5edd8a649910c93efaed59a982
Md Nouman <noumantannu06@gmail.com>
2026-06-13 20:23:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	// Remove all non-alphanumeric characters and convert to lowercase
+  const cleaned = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+  // Create reversed string
+  const reversed = cleaned.split('').reverse().join('');
+  
+  // Check if cleaned string equals reversed string
+  return cleaned === reversed;
 
 }
 module.exports = palindrome
